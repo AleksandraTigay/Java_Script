090ceea83d8aab101132a477ee6dbd6054fb1498
AleksandraTigay <aleksandra.tigay002@gmail.com>
2023-11-21 18:01:05 +0200

    HW_1

---
+++ Lesson_1/hw_1.js
@@ -0,0 +1,43 @@
+// task_1
+let a1 = `hello`
+console.log(a1)
+let b1 = `owu`
+console.log(b1)
+let c1 = `ua`
+console.log(c1)
+
+let a2 = 1
+console.log(a2)
+let b2 = 10
+console.log(b2)
+let c2 = -999
+console.log(c2)
+let d = 123
+console.log(d)
+let e = 3.14
+console.log(e)
+let f = 2.7
+console.log(f)
+let g = 16
+console.log(g)
+
+let a3 = true
+console.log(a3)
+let b3 = false
+console.log(b3)
+
+
+// task_2
+let firstName  = `Stephen`
+let middleName  = `Edwin`
+let lastName  = `King`
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+
+// task_3
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
